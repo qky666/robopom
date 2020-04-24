0f79b94d4c973716d7454245d9a55322887e4051
jose.torrecilla@gmail.com <jose.torrecilla@gmail.com>
2020-04-24 09:02:15 +0200

    Distribution

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='robopom',
     version='0.1.0',
-    description='robopom package',
+    description='Page Object Model for Robot Framework',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='José Torrecilla Álvarez',
-    author_email='jose.torrecilla@mtp.es',
+    author_email='jose.torrecilla@gmail.com',
     license='Apache 2.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0",
+        "Operating System :: OS Independent",
+        "Framework :: Robot Framework",
+        "Framework :: Robot Framework :: Library",
+        "Environment :: Web Environment",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+    ],
     url="https://github.com/qky666/robopom",
+    python_requires='>=3.7',
     packages=setuptools.find_packages(),
     package_data={'robopom.resources': ['*'],
                   'robopom.resources.template_files': ['*'], },
